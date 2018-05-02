8c4e721993cf230658007d1ecf05dca8561e9a79
Avalander <darth.quesorico@gmail.com>
2018-05-02 16:06:12 +0200

    Add snabbdom.

--- web/index.js
+++ web/index.js
@@ -1,1 +1,17 @@
-document.querySelector('#root').innerHTML = '<h1>Hello World!</h1>'
+import { init, h } from 'snabbdom'
+import snabbdom_class from 'snabbdom/es/modules/class'
+import snabbdom_props from 'snabbdom/es/modules/props'
+import snabbdom_style from 'snabbdom/es/modules/style'
+import snabbdom_eventlisteners from 'snabbdom/es/modules/eventlisteners'
+
+const patch = init([
+	snabbdom_class,
+	snabbdom_props,
+	snabbdom_style,
+	snabbdom_eventlisteners,
+])
+
+const root = document.querySelector('#root')
+const view = h('h1', 'Hello world!')
+
+patch(root, view)
